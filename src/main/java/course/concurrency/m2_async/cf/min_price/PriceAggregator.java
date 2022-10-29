4f1b9eb0d3eb684de08204f30f75c58b6bea8929
railem <rkhisamutdinov@cloudally.com>
2022-10-29 12:23:21 +0300

    МПJ(2-3/3) Практика: возврат наименьшей цены исправление

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -1,41 +1,39 @@
 package course.concurrency.m2_async.cf.min_price;
 
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 public class PriceAggregator {
 
-    private PriceRetriever priceRetriever = new PriceRetriever();
+	private PriceRetriever priceRetriever = new PriceRetriever();
 
-    public void setPriceRetriever(PriceRetriever priceRetriever) {
-        this.priceRetriever = priceRetriever;
-    }
+	public void setPriceRetriever(PriceRetriever priceRetriever) {
+		this.priceRetriever = priceRetriever;
+	}
 
-    private Collection<Long> shopIds = Set.of(10l, 45l, 66l, 345l, 234l, 333l, 67l, 123l, 768l);
+	private Collection<Long> shopIds = Set.of(10l, 45l, 66l, 345l, 234l, 333l, 67l, 123l, 768l);
 
-    public void setShops(Collection<Long> shopIds) {
-        this.shopIds = shopIds;
-    }
+	public void setShops(Collection<Long> shopIds) {
+		this.shopIds = shopIds;
+	}
 
 	public double getMinPrice(long itemId) {
-		List<CompletableFuture<Double>> minPricesCF = shopIds.stream()
+		CompletableFuture<Double>[] minPricesCF = shopIds.stream()
 				.map(shopId -> CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(itemId, shopId)))
-				.map(f -> f.completeOnTimeout(Double.NaN, 3, TimeUnit.SECONDS))
-				.map(f -> f.exceptionally(e -> Double.NaN))
-				.collect(Collectors.toList());
+				.map(f -> f.orTimeout(2900, TimeUnit.MILLISECONDS).exceptionally(e -> null))
+				.toArray(CompletableFuture[]::new);
 
-		CompletableFuture<Void> resultantCf = CompletableFuture.allOf(
-				minPricesCF.toArray(new CompletableFuture[minPricesCF.size()])
-		);
-		return resultantCf.thenApply(
-				t -> minPricesCF.stream()
+		return CompletableFuture.allOf(minPricesCF).thenApply(
+				t -> Arrays.stream(minPricesCF)
 						.map(CompletableFuture::join)
-						.min(Double::compareTo)
+						.filter(Objects::nonNull)
+						.mapToDouble(r -> r)
+						.min()
 						.orElse(Double.NaN)
-		).getNow(Double.NaN);
+		).join();
 	}
 }
